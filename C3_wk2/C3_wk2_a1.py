1d13bbfa0c367c7cb52795eb229d945a7c4397dd
pratt3000 <pratt3000@gmail.com>
2020-05-30 08:29:36 +0530

    reformatted

--- C3_wk2/C3_wk2_a1.py
+++ C3_wk2/C3_wk2_a1.py
@@ -1,7 +1,8 @@
 import re
-file = open("regex_sum_588840.txt") #or "regex_sum_588840.txt"
+
+file = open("regex_sum_588840.txt")  # or "regex_sum_588840.txt"
 file = file.read()
-y = re.findall('[0-9]+', file)
+y = re.findall("[0-9]+", file)
 sum = 0
 for i in y:
     num = int(i)
--- C3_wk4/assignment2.py
+++ C3_wk4/assignment2.py
@@ -7,18 +7,18 @@ ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
-url = 'http://py4e-data.dr-chuck.net/known_by_Bayley.html'
+url = "http://py4e-data.dr-chuck.net/known_by_Bayley.html"
 html = urlopen(url, context=ctx).read()
 soup = BeautifulSoup(html, "html.parser")
 
 # Retrieve all of the anchor tags
 
-    # Look at the parts of a tag
-for i in range(0,7):
-    tags = soup('a')
+# Look at the parts of a tag
+for i in range(0, 7):
+    tags = soup("a")
     tag = tags[17]
     print(tag.contents[0])
 
-    url = tag.get('href', None)
+    url = tag.get("href", None)
     html = urlopen(url, context=ctx).read()
     soup = BeautifulSoup(html, "html.parser")
